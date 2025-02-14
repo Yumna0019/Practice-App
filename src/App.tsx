8bc01e404fcbdbdc1531b6934011db2652e95089
Yumna Mubeen <yumnamubeen0019@gamil.com>
2025-02-14 19:42:20 +0500

    git push

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import './App.css'
 function App() {
 
   return (
-    <>
+    <div>
       <h1 className='text-amber-800'>Vite + Yumna + React</h1>
       <h2 className='hehe'>classs same </h2>
       <h3 className='hehe'>classs same </h3>
-    </>
+    </div>
   )
 }
 
