efc32c1d47b80ba346d441abe1fde475c6dae5cc
Yumna Mubeen <yumnamubeen0019@gamil.com>
2025-02-21 12:27:02 +0500

    counter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 // import { useState } from 'react'
 import './App.css'
+import Counter from './components/Counter'
 
 function App() {
 
   return (
     <div>
-      <h1 className='text-amber-800'>Vite + Yumna + React</h1>
-      <h2 className='hehe'>classs same </h2>
-      <h3 className='hehe'>classs same </h3>
+      <Counter />
+      <Counter />
     </div>
   )
 }
---
+++ src/components/Counter.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+const Counter = () => {
+    const [cvalue , setCvalue] = useState(0);
+
+        
+    return ( 
+        <section className="flex flex-col items-center gap-4 p-6 bg-gray-100 rounded-xl shadow-lg w-80">
+            <h1 className="text-2xl font-semibold">Counter = {cvalue}</h1>
+            <div className="flex gap-3 border border-gray-300 p-3 rounded-lg bg-white">
+                <button
+                    className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition"
+                    onClick={() => {setCvalue(cvalue -1)} }
+                >
+                    - Dec
+                </button>
+                <button
+                    className="px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600 transition"
+                    onClick={() => {setCvalue(cvalue +1)} }
+                >                
+                    + Inc
+                </button>
+                <button
+                    className="px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600 transition"
+                    onClick={() => {setCvalue(0)} }
+                >
+                    Reset
+                </button>
+            </div>
+        </section>
+     );
+}
+ 
+export default Counter;
